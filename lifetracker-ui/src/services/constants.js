27237a9918753120674843129850c002b0fe67c6
Bosch <boschio91@gmail.com>
2022-07-08 14:00:53 -0400

    Updated constants URL

--- lifetracker-ui/src/services/constants.js
+++ lifetracker-ui/src/services/constants.js
@@ -1,5 +1,5 @@
 
-export const PRODUCTION_API_BASE_URL="http://localhost:3001"
+export const PRODUCTION_API_BASE_URL="https://bosch-lifetracker.herokuapp.com/"
 export const DEVELOPMENT_API_BASE_URL="http://localhost:3001"
 const API_BASE_URL=DEVELOPMENT_API_BASE_URL
 
