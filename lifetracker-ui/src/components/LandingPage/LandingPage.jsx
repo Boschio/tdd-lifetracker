f605fd58f9e4fe4b4f87360c44623a8687fc2bb9
Bosch <boschio91@gmail.com>
2022-07-08 20:34:30 -0400

    Fixed issue with surge deployment where logo and background image would not appear.

--- lifetracker-ui/src/components/LandingPage/LandingPage.jsx
+++ lifetracker-ui/src/components/LandingPage/LandingPage.jsx
@@ -1,13 +1,12 @@
 import * as React from "react"
+import heroImg from "../../../media/smartwatch-screen-digital-device.svg"
 import "./LandingPage.css"
 
 export default function LandingPage() {
     return (
         <div className="landing-page">
             <div className="hero">
-                {/* <div className="hero-img"> */}
-                    <img className="hero-img" src="./media/smartwatch-screen-digital-device.svg" alt="hero-img"/>
-                {/* </div> */}
+                    <img className="hero-img" src={heroImg} alt="hero-img"/>
                 <div className="cta">
                     <h1 className="header">LifeTracker</h1>
                     <p>Helping you take back control of your world</p>
--- lifetracker-ui/src/components/Navbar/Navbar.jsx
+++ lifetracker-ui/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "react-router-dom"
 import NavLinks from "components/NavLinks/NavLinks"
+import logo from "../../../media/giant_codepath_Logo.svg"
 import "./NavBar.css"
 
 export default function Navbar() {
@@ -8,7 +9,7 @@ export default function Navbar() {
     return (
         <nav className="navbar">
                 <div className="logo">
-                    <Link to="/"><img src="./media/giant_codepath_Logo.svg" alt="logo"/></Link>
+                    <Link to="/"><img src={logo} alt="logo"/></Link>
                 </div>
                 <div className="links">
                     <NavLinks />
